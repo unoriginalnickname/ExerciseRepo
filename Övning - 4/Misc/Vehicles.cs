d87a5b8ac94d5aaaa4ac6999016c08c2370c5045
unoriginalnickname <aadammatthews@gmail.com>
2026-05-22 10:16:46 +0200

    removed code duplication for the vehicle classes

--- Övning - 4/Misc/Vehicles.cs
+++ Övning - 4/Misc/Vehicles.cs
@@ -1,65 +1,17 @@
-﻿    public class Airplane : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
-    public class Motorcycle : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
-    public class Car : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
+﻿public abstract class VehicleBase : IVehicle
+{
+    public required string RegistryNumber { get; set; }
+    public required string Color { get; set; }
+    public required int NumWheels { get; set; }
+    public required string FuelType { get; set; }
+    public required string UniquePropertyValue { get; set; }
+    public required string UniquePropertyString { get; set; }
+}
 
-    public class Bus : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
-    public class Boat : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
-    public class Ufo : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
-    public class Uap : IVehicle
-    {
-        public string RegistryNumber { get; set; }
-        public string Color { get; set; }
-        public int NumWheels { get; set; }
-        public string FuelType { get; set; }
-        public string UniquePropertyValue { get; set; }
-        public string UniquePropertyString { get; set; }
-    }
-
+public class Airplane : VehicleBase {  } 
+public class Motorcycle : VehicleBase {  } 
+public class Car : VehicleBase {  }  
+public class Bus : VehicleBase {  } 
+public class Boat : VehicleBase {  }
+public class Ufo : VehicleBase {  } 
+public class Uap : VehicleBase {  } 
